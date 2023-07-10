cbd20823f89ec0d508396d8fafca0467b16e9979
m.kiida <mitsuo1110@gmail.com>
2023-07-10 21:50:35 +0900

    01完成

--- 01.go
+++ 01.go
@@ -2,17 +2,28 @@ package main
 
 // メイン関数
 func main() {
-	var ans = nlp01("stressed")
-	print(ans)
+
+	var ans1 = nlp00("stressed")
+	println(ans1)
+
+	var ans2 = nlp01("パタトクカシーー")
+	println(ans2)
 }
 
 // 00. 文字列の逆順
-func nlp01(s string) string {
+func nlp00(s string) string {
 	rs := []rune(s)
 
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		rs[i], rs[j] = rs[j], rs[i]
 	}
+
+	return string(rs)
+}
+
+// 01. 「パタトクカシーー」
+func nlp01(s string) string {
+	rs := []rune(s)
 
 	return string(rs)
 }
